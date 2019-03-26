a8a596ab9d90c5d527a759a92e31f8ded78a122b
Michaël Zanatta <michael.zanatta@student.ecp.fr>
2019-03-27 00:55:01 +0100

    update eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     "extends": "airbnb-base",
     "rules": {
         // Indent with 4 spaces
-    "indent": ["error", 4]
+    "indent": ["error", 4],
+    "max-len": [2, {"code": 125}]
     }
 };
